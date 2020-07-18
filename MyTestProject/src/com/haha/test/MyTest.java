3a763b902a73d50be901fdc951449e6fa3c7c95a
Lanxlove <610516836@qq.com>
2020-07-18 21:27:34 +0800

    xiangceshi chongtu

--- MyTestProject/src/com/haha/test/MyTest.java
+++ MyTestProject/src/com/haha/test/MyTest.java
@@ -3,6 +3,8 @@ package com.haha.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println(111111);
+        int y = 1;
         int i = 1;
+        int c = 1;
     }
 }
