ae47883260c32da1dfda8c6cd1b1a37657815ab5
Lanxlove <610516836@qq.com>
2020-07-18 21:29:10 +0800

    修改了代码

--- MyTestProject/src/com/haha/test/MyTest.java
+++ MyTestProject/src/com/haha/test/MyTest.java
@@ -3,8 +3,9 @@ package com.haha.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println(111111);
+        int b = 1;
+        int z = 2;
         int y = 1;
-        int i = 1;
         int c = 1;
     }
 }
